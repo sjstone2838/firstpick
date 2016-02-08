518bc4629e5a632037fde3e765b2f8e416dd0513
Samuel Stone <samstone@Samuels-MacBook-Air.local>
2016-02-07 21:50:32 -0500

    initial Heroku commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
