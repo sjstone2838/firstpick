b6429409652b70522f62ff83e6171feffbb61b12
Samuel Stone <samstone@Samuels-MacBook-Air.local>
2016-02-12 10:34:22 -0500

    Celery back on with AMQP

--- mysite/celery.py
+++ mysite/celery.py
@@ -33,7 +33,7 @@ def send_event_reminders(self):
 	# IF RUN-TIME IS NON-TRIVIAL AND ONLY ONE CELERY WORKER (THREAD),
 	# THEN THIS MAY LEAVE GAPS FOR EVENTS WHERE NOTIFICATIONS NOT SENT
 	PRECEED_BY = 60 #minutes
-	WINDOW = 15 # minutes
+	WINDOW = 15 #minutes
 	now = datetime.datetime.now()
 	start = now + timedelta(minutes = PRECEED_BY)
 	end = now + timedelta(minutes = PRECEED_BY + WINDOW)
